333e0351472013ef3aec5cbd1d3f4b23fd27c3dc
asmaa <mmukulkumar@live.com>
2022-02-10 13:40:20 +0000

    up

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,10 @@
 require("@nomiclabs/hardhat-waffle");
 const fs = require('fs');
-// const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789";
-// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+const privateKey = fs.readFileSync(".secret").toString().trim() || "77e443de0ff7652636f22f0b7b40f50ef0c005317a3b6ee9c9aa4c4de34da4d3";
+
+// infuraId is optional if you are using Infura RPC
+const ganache = fs.readFileSync(".ganache").toString().trim() || "77e443de0ff7652636f22f0b7b40f50ef0c005317a3b6ee9c9aa4c4de34da4d3";
+const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
 
 module.exports = {
   defaultNetwork: "hardhat",
@@ -9,7 +12,6 @@ module.exports = {
     hardhat: {
       chainId: 1337
     },
-    /*
     mumbai: {
       // Infura
       // url: `https://polygon-mumbai.infura.io/v3/${infuraId}`
@@ -22,7 +24,6 @@ module.exports = {
       url: "https://rpc-mainnet.maticvigil.com",
       accounts: [privateKey]
     }
-    */
   },
   solidity: {
     version: "0.8.4",
@@ -34,4 +35,3 @@ module.exports = {
     }
   }
 };
-
